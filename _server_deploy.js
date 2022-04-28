f32cf9d2097dcde297468d97ef75979fc908f93e
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-04-28 08:25:32 +0600

    added server deploy some steps

---
+++ _server_deploy.js
@@ -0,0 +1,14 @@
+/* 
+*server deploy
+----------------
+one time for computer
+1.create heroku account
+2.verify account
+3.install heroku cli
+4.heroku login
+----------------
+each project for one time:
+1.heroku create
+2.make sure you: git add . git commit . git push
+3.git push heroku main.
+*/
