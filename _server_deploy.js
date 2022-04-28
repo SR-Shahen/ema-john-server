131f7f7b9da2f8bcf588eacd68fbb8dfd7958f49
SR-Shahen <s.r.shahenda@gmail.com>
2022-04-28 11:56:04 +0600

    server deploy added

--- _server_deploy.js
+++ _server_deploy.js
@@ -11,4 +11,6 @@ each project for one time:
 1.heroku create
 2.make sure you: git add . git commit . git push
 3.git push heroku main.
+4.Go to heroku dashboard> Current project> Reval config.
+5. copy paste config vars from .env
 */
